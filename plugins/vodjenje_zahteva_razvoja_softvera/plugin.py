209efec0de123f4c9a344ab19509f43966885461
Toma1997 <toma.joksimovic@gmail.com>
2019-01-29 22:43:56 +0100

    Added main plugin for plugin framework

---
+++ plugins/vodjenje_zahteva_razvoja_softvera/plugin.py
@@ -0,0 +1,25 @@
+from plugin_framework.plugin import Plugin
+
+class Main(Plugin):
+    """
+    Klasa koja predstavlja konkretni plugin. Nasledjujemo "apstraktnu" klasu Plugin.
+    Ova klasa predstavlja plugin za aplikaciju zahteva o razvoju softvera.
+    """
+    def __init__(self, spec):
+        """
+        Inicijalizator Razvoj Softvera plugina.
+
+        :param spec: specifikacija metapodataka o pluginu.
+        :type spec: dict
+        """
+        super().__init__(spec)
+
+    def get_widget(self, parent=None):
+        """
+        Ova metoda vraca konkretni widget koji ce biti smesten u centralni deo aplikacije i njenog 
+        glavnog prozora. Može da vrati toolbar, kao i meni, koji će biti smešten u samu aplikaciju.
+        
+        :param parent: bi trebao da bude widget u koji će se smestiti ovaj koji naš plugin omogućava.
+        :returns: QWidget, QToolbar, QMenu
+        """
+        return None, None, None
