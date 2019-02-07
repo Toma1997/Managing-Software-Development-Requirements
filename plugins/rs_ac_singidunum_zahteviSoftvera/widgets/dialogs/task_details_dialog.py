cd225e15b16da51bcc59e87df9e77dea097e0822
Toma1997 <toma.joksimovic@gmail.com>
2019-02-07 22:14:19 +0100

    Repaired details vertical header

--- plugins/rs_ac_singidunum_zahteviSoftvera/widgets/dialogs/task_details_dialog.py
+++ plugins/rs_ac_singidunum_zahteviSoftvera/widgets/dialogs/task_details_dialog.py
@@ -22,7 +22,7 @@ class TaskDetailsDialog(QtWidgets.QDialog):
         super().__init__(parent)
         self.setWindowTitle("Detalji odabranog zadatka")
         # prosiriv ekran velicina
-        self.resize(1200, 540)
+        self.resize(1200, 520)
         # postavljanje ikonice prozora
         self.setWindowIcon(QtGui.QIcon("resources/icons/application-detail.png")) 
 
@@ -33,32 +33,32 @@ class TaskDetailsDialog(QtWidgets.QDialog):
         self.personal_tasks_dialog_layout = QtWidgets.QVBoxLayout()
 
         self.tasks_table = QtWidgets.QTableWidget(self)
+        self.tasks_table.horizontalHeader().setVisible(False)
         self.tasks_table.verticalHeader().setVisible(True)
+        self.tasks_table.verticalHeader().setSortIndicatorShown(True)
         self.tasks_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.tasks_table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         self.tasks_table.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+        self.tasks_table.setGridStyle(QtCore.Qt.SolidLine)
+        self.tasks_table.setAlternatingRowColors(True)
 
         self._populate_table()
 
         self.tasks_table.verticalHeader().setStretchLastSection(True)
         self.personal_tasks_dialog_layout.addWidget(self.tasks_table)
         self.setLayout(self.personal_tasks_dialog_layout)
-        
+        self.tasks_table.setSortingEnabled(True)
     def _populate_table(self):
         """
         Popunjava tabelu sa podacima za licne zadatke.
         """
         self.tasks_table.clear()
         self.tasks_table.setColumnCount(1)
+        self.tasks_table.setRowCount(len(self.task_details))
         self.tasks_table.setVerticalHeaderLabels(
-               ["Naziv zadatka", "Ceo opis", "Naziv labele", "Boja labele", "Autor labele", "Email autora labele", "Pozicija autora labele",
-                "Autor zadatka", "Email autora zadatka", "Pozicija autora zadatka", "Datum kreiranja", "Datum prihvatanja",
-                 "Prihvatio korisnik", "Email prihvataca zadatka", "Pozicija prihvataca zadatka" "Status"])
+               ["Naziv zadatka", "Ceo opis", "Naziv labele", "Boja labele", "Autor labele", "Email autora labele", "Pozicija autora labele", "Autor zadatka", "Email autora zadatka", "Pozicija autora zadatka", "Datum kreiranja", "Datum prihvatanja", "Prihvatio korisnik", "Email prihvataca zadatka", "Pozicija prihvataca zadatka", "Status"])
         self.tasks_table.setColumnWidth(0, 1160)
         
-        self.tasks_table.setRowCount(len(self.task_details))
-
         for i, detail in enumerate(self.task_details):
             detailItem = QtWidgets.QTableWidgetItem(detail)
             self.tasks_table.setItem(i, 0, detailItem)
-    
